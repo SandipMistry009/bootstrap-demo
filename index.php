964c067865b504f4ac14a928b1f0483dc39a0a24
SandipMistry009 <mistry.sandi009@gmail.com>
2020-03-16 10:31:19 +0530

    database file added and index file changes uploaded

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My Demo Project</title>
+	<title>My Demo Project using Bootstrap</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
